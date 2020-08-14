912d32a28ae403f140c6035080deb90f3f77f2cc
DavidBlazek18 <67991822+DavidBlazek18@users.noreply.github.com>
2020-08-13 23:51:24 -0400

    Add files via upload

---
+++ Basic_HTML_2/JS/Basic_JavaScript_2 - myFIrstFunction.js
@@ -0,0 +1,23 @@
+
+
+function My_First_Function() {
+	
+	var string=("This text is green");
+	var result=str.fontcolor("green");
+document.getElementById("Green_Text").innerHTML=result;
+}
+//The getElementById() method returns the element 
+//that has the ID attribute with the specified value. 
+//This method is one of the most common methods in the 
+//HTML DOM, and is used almost every time you want to 
+//manipulate, or get info from, an element on your document.
+
+//As a note: innerHTML modifies the content of an HTML element.
+//innerHTML is a property of every element. 
+//It tells you what is between the starting 
+//and ending tags of the element, and it 
+//also let you sets the content of the 
+//element. ... Each HTML element has an 
+//innerHTML property that defines both 
+//the HTML code and the text that occurs 
+//between that element's opening and closing tag.
