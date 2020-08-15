a8004a0b984928cadba5c8cdebef5e53ea80f38e
DavidBlazek18 <67991822+DavidBlazek18@users.noreply.github.com>
2020-08-15 13:17:20 -0400

    Add files via upload
    Push Basic_HTML_4 to GitHub per assignment on Page 92.

---
+++ Basic_HTML_4/JS/Basic_JavaScript_4.js
@@ -0,0 +1,27 @@
+
+
+function exampleDictionary() {
+	var beer = {
+		type:"Lager",
+		alcoholContent:"5.9%",
+		packageType:"12-pack",
+		packageMaterial:"aluminum",
+		cost:"$16.99"
+		};
+	document.getElementById("Dictionary").innerHTML = beer.alcoholContent;
+}
+//The getElementById() method returns the element 
+//that has the ID attribute with the specified value. 
+//This method is one of the most common methods in the 
+//HTML DOM, and is used almost every time you want to 
+//manipulate, or get info from, an element on your document.
+
+//As a note: innerHTML modifies the content of an HTML element.
+//innerHTML is a property of every element. 
+//It tells you what is between the starting 
+//and ending tags of the element, and it 
+//also let you sets the content of the 
+//element. ... Each HTML element has an 
+//innerHTML property that defines both 
+//the HTML code and the text that occurs 
+//between that element's opening and closing tag.
